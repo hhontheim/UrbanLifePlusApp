aaa3bbaa491556ccadf2986c2aaf3230f443be39
Henning Hontheim <henning.hontheim@unibw.de>
2020-05-14 14:19:42 +0200

    Update UI
    Signed-off-by: Henning Hontheim <henning.hontheim@unibw.de>

--- UrbanLifePlusApp WatchKit Extension/Views/ContentView.swift
+++ UrbanLifePlusApp WatchKit Extension/Views/ContentView.swift
@@ -11,35 +11,60 @@ import SwiftUI
 struct ContentView: View, SessionCommands {
     @EnvironmentObject var userData: UserData
     
+    @State var didReceiveInitialDataFromPhone: Bool = !false
     
     var body: some View {
         //        ZStack {
         //            Text("Bitte öffne zuerst die App auf deinem iPhone um dich anmelden...")
         //        }
-        ScrollView {
-            Text("Value: \"\(userData.value)\"")
-            TextField("Text Feld", text: $userData.value)
-            Toggle(isOn: $userData.toggleMessage) {
-                Text("Toggle me")
-            }
-            .padding()
-            Toggle(isOn: $userData.toggleUserInfo) {
-                Text("Toggle me")
-            }
-            .padding()
-            Button(action: sendMessage) {
-                Text("Send Message")
-            }
-            Button(action: sendUserInfo) {
-                Text("Send UserInfo")
+        ZStack {
+            if didReceiveInitialDataFromPhone {
+                ScrollView {
+                    Text("Value: \"\(userData.value)\"")
+                    TextField("Text Feld", text: $userData.value)
+                    Toggle(isOn: $userData.toggleMessage) {
+                        Text("Toggle me")
+                    }
+                    .padding()
+                    Toggle(isOn: $userData.toggleUserInfo) {
+                        Text("Toggle me")
+                    }
+                    .padding()
+                    Button(action: sendMessage) {
+                        Text("Send Message")
+                    }
+                    Button(action: sendUserInfo) {
+                        Text("Send UserInfo")
+                    }
+                }
+                .onAppear {
+                    return
+                }
+                .navigationBarTitle("Home")
+            } else {
+                Group {
+                    Color.black
+                        .edgesIgnoringSafeArea(.all)
+                    VStack {
+                        Text("Bitte öffne zuerst die App auf deinem iPhone um dich anmelden...")
+                        Button(action: {}) {
+                            Text("Neu laden...")
+                        }
+                    }
+                }
+                .onAppear {
+                    self.requestUserDataFromPhone()
+                }
+                .navigationBarTitle("UrbanLife+")
             }
         }
-        .onAppear {
-            return
-        }
-        .navigationBarTitle("Home")
     }
     
+    func requestUserDataFromPhone() {
+//        sendUserInfoMessage([
+//            StorageKey.value : userData.requestDataFromPhone
+//        ])
+    }
     
     func sendMessage() {
         sendMessage([
